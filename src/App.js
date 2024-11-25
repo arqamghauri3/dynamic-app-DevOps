be15e3a672222a06a02ab1082e240629824b6085
Arqam Habib Ghauri <arqamghauri3@gmail.com>
2024-11-25 20:45:13 +0500

    updated version

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Dynamic Website DevOps Assignment 1</h1>
+      <h1>Dynamic Website DevOps Assignment 1 V1</h1>
     </div>
   );
 }
